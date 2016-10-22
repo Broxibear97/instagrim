0e225a5e3f0a5c1b8abc6ed9bf7b571b6f30e68f
joshuahogarth <joshuahogarth@labpc163.computing.dundee.ac.uk>
2016-10-22 23:08:25 +0100

    calls function to retrieve details for current profile being viewed. if there are details for that user, it forwards it to the jsp page.

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Profile.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Profile.java
@@ -6,6 +6,7 @@
 package uk.ac.dundee.computing.aec.instagrim.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,12 +14,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
+import uk.ac.dundee.computing.aec.instagrim.models.Profiler;
 
 /**
  *
  * @author joshuahogarth
  */
-@WebServlet(name = "Profile", urlPatterns = {"/Profile", "/Profile/*"})
+@WebServlet(name = "Profile", urlPatterns = {"/Profile", "/Profile/*", "/profile", "/profile/*"})
 public class Profile extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -36,6 +38,12 @@ public class Profile extends HttpServlet {
             
         String args[] = Convertors.SplitRequestPath(request);
         request.setAttribute("user", args[2]);
+        
+        Profiler prof = new Profiler();
+        ArrayList<String> details = prof.RetrieveDetails(args[2]);
+        if(details != null)
+            request.setAttribute("details", details);
+        
         RequestDispatcher rd = request.getRequestDispatcher("/userprofile.jsp");
         rd.forward(request, response);
     }
